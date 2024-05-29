2a0becd2420e7d8a695d3456da077553fc8e0e75
Aaryan Purohit <ap1922@ic.ac.uk>
2024-05-29 17:27:27 +0100

    [ap] Removed unnecessary comments in marketplace

--- lib/screens/marketplace.dart
+++ lib/screens/marketplace.dart
@@ -2,46 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../item_provider.dart';
 
-// class Marketplace extends StatelessWidget {
-//   const Marketplace({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final items = Provider.of<ItemProvider>(context).items;
-
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Marketplace'),
-//       ),
-//       body: SingleChildScrollView(
-//         child: DataTable(
-//           columns: const [
-//             DataColumn(label: Text('Name')),
-//             DataColumn(label: Text('Photo')),
-//             DataColumn(label: Text('Price')),
-//             DataColumn(label: Text('Expiry Date')),
-//           ],
-//           rows: items.map((item) {
-//             return DataRow(cells: [
-//               DataCell(Text(item.name)),
-//               DataCell(item.photos.isNotEmpty
-//                   ? Image.file(
-//                       item.photos[0],
-//                       width: 50,
-//                       height: 50,
-//                       fit: BoxFit.cover,
-//                     )
-//                   : Container()),
-//               DataCell(Text(item.price)),
-//               DataCell(Text(item.expiryDate)),
-//             ]);
-//           }).toList(),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 class Marketplace extends StatelessWidget {
   const Marketplace({super.key});
 
