5a7a4dab645f5f771482a97d6f579e5dd19bc9c4
Rohan Shah <rohanshah.13@hotmail.co.uk>
2024-05-30 09:40:22 +0100

    [rs] - (feat) All pictures can be viewed in the detailed scaffold

--- lib/screens/item_detail_page.dart
+++ lib/screens/item_detail_page.dart
@@ -15,35 +15,63 @@ class ItemDetailPage extends StatelessWidget {
       appBar: AppBar(title: Text(item.name)),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: ListView(
           children: [
-            kIsWeb ? Image.network(item.photos[0].path, width: screenWidth*0.15, height: screenWidth*0.15, fit: BoxFit.cover,):
-            Image.file(
-              item.photos[0],
-              width: screenWidth,
-              height: screenWidth, // Maintain aspect ratio
-              fit: BoxFit.cover,
-            ),
-            const SizedBox(height: 10),
-            Text(
-              item.name,
-              style: TextStyle(fontSize: screenWidth * 0.05), // Responsive text size
-            ),
-            const SizedBox(height: 10),
-            Text(
-              "£${item.price}",
-              style: TextStyle(fontSize: screenWidth * 0.04), // Responsive text size
-            ),
-            const SizedBox(height: 30),
-            Text(
-              item.description,
-              style: TextStyle(fontSize: screenWidth * 0.04), // Responsive text size
-            ),
-            const SizedBox(height: 10),
-            Text(
-              item.expiryDate,
-              style: TextStyle(fontSize: screenWidth * 0.04), // Responsive text size
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Display all photos
+                SizedBox(
+                  height: screenWidth, // Adjust the height as needed
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: item.photos.length,
+                    itemBuilder: (context, index) {
+                      final photo = item.photos[index];
+                      return Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: kIsWeb
+                            ? Image.network(
+                                photo.path,
+                                width: screenWidth * 0.15,
+                                height: screenWidth * 0.15,
+                                fit: BoxFit.cover,
+                              )
+                            : Image.file(
+                                photo,
+                                width: screenWidth,
+                                height: screenWidth, // Maintain aspect ratio
+                                fit: BoxFit.cover,
+                              ),
+                      );
+                    },
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  item.name,
+                  style: TextStyle(
+                      fontSize: screenWidth * 0.05), // Responsive text size
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  "£${item.price}",
+                  style: TextStyle(
+                      fontSize: screenWidth * 0.04), // Responsive text size
+                ),
+                const SizedBox(height: 30),
+                Text(
+                  item.description,
+                  style: TextStyle(
+                      fontSize: screenWidth * 0.04), // Responsive text size
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  item.expiryDate,
+                  style: TextStyle(
+                      fontSize: screenWidth * 0.04), // Responsive text size
+                ),
+              ],
             ),
           ],
         ),
